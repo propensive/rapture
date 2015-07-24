f94c04baf67df8d98e50014ea99324ac6ddabcaf
Raúl Raja Martínez <raulraja@gmail.com>
2015-07-24 11:07:08 +0200

    Improved `ResultT` monad transformer to be invariant and accumulate errors in the type signature when diverging on `E <: Exception` composing with other Results in the same way `Result` does.

--- core-scalaz/shared/src/main/scala/transformers.scala
+++ core-scalaz/shared/src/main/scala/transformers.scala
@@ -17,47 +17,52 @@ import scalaz.{Functor, _}
  * ResultT(x).map(1+).run // Some(Answer(2))
  * }}}
  **/
-sealed trait ResultT[F[+ _], +T, E <: Exception] {
+sealed trait ResultT[F[_], T, E <: Exception] {
 
   val run: F[Result[T, E]]
 
   /** Map on the answer of this result. */
-  def map[C](f: T => C)(implicit functor: Functor[F], cte : ClassTag[E]): ResultT[F, C, E] =
+  def map[C](f: T => C)(implicit functor: Functor[F], cte: ClassTag[E]): ResultT[F, C, E] =
     ResultT(functor.map(run)(_.map(f)))
 
-  /** Bind through the answer of this result. */
-  def flatMap[C](f: T => ResultT[F, C, E])(implicit functor: Monad[F], cte : ClassTag[E]): ResultT[F, C, E] =
-    ResultT[F, C, E](functor.bind(run)(_.fold(a => f(a).run, b => functor.point(Errata[C, E](b)))))
+  /** Bind through the answer of this result accumulating errors in the contents and type signature. */
+  def flatMap[C, E2 <: Exception](f: T => ResultT[F, C, E2])(implicit monad: Monad[F], cte: ClassTag[E2]): ResultT[F, C, E with E2] = {
+    ResultT(monad.bind[Result[T, E], Result[C, E with E2]](run) { result =>
+      result.fold[F[Result[C, E with E2]]]({ a =>
+        monad.map(f(a).run)( r2 => Result[C, E with E2](r2.get, r2.errors ++ result.errors))
+      },{ e =>
+        monad.point(Errata[C, E with E2](e))
+      })
+    })
+  }
 
   /** Filter on the answer of this result. */
-  def filter(p: T => Boolean)(implicit functor: Functor[F], cte : ClassTag[E]): ResultT[F, T, E with NotMatchingFilter] =
+  def filter(p: T => Boolean)(implicit functor: Functor[F], cte: ClassTag[E]): ResultT[F, T, E with NotMatchingFilter] =
     ResultT(functor.map(run)(_.filter(p)))
 
-  /** Alias for `filter`.
-    * @since 7.0.2
-    */
-  def withFilter(p: T => Boolean)(implicit functor: Functor[F], cte : ClassTag[E]): ResultT[F, T, E with NotMatchingFilter] =
+  /** Alias for `filter` */
+  def withFilter(p: T => Boolean)(implicit functor: Functor[F], cte: ClassTag[E]): ResultT[F, T, E with NotMatchingFilter] =
     filter(p)
 
 }
 
 object ResultT extends ResultTFunctions {
   /** Construct a result value. */
-  def apply[F[+ _], T, E <: Exception : ClassTag](a: F[Result[T, E]]): ResultT[F, T, E] =
+  def apply[F[_], T, E <: Exception : ClassTag](a: F[Result[T, E]]): ResultT[F, T, E] =
     resultT[F, T, E](a)
 
   /** Construct an answer value. */
-  def answer[F[+ _], T, E <: Exception : ClassTag](a: F[T])(implicit functor: Functor[F]): ResultT[F, T, E] =
+  def answer[F[_], T, E <: Exception : ClassTag](a: F[T])(implicit functor: Functor[F]): ResultT[F, T, E] =
     apply[F, T, E](functor.map(a)(Result.answer[T, E]))
 
   /** Construct an errata value. */
-  def errata[F[+ _], T, E <: Exception : ClassTag](a: F[E])(implicit functor: Functor[F]): ResultT[F, T, E] =
+  def errata[F[_], T, E <: Exception : ClassTag](a: F[E])(implicit functor: Functor[F]): ResultT[F, T, E] =
     apply[F, T, E](functor.map(a)(Result.errata[T, E]))
 
 }
 
 private[scalazInterop] trait ResultTFunctions {
-  def resultT[F[+ _], T, E <: Exception](a: F[Result[T, E]]): ResultT[F, T, E] = new ResultT[F, T, E] {
+  def resultT[F[_], T, E <: Exception](a: F[Result[T, E]]): ResultT[F, T, E] = new ResultT[F, T, E] {
     val run = a
   }
 }
