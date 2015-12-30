aa28c2c7df79766fa0e0805c83988b40d3641442
Jon Pretty <jon.pretty@propensive.com>
2015-12-30 04:01:18 +0000

    Renamed rapture.web package to rapture.http

--- http/shared/src/main/scala/handlers.scala
+++ http/shared/src/main/scala/handlers.scala
@@ -10,7 +10,7 @@
 * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License    *
 * for the specific language governing permissions and limitations under the License.                               *
 \******************************************************************************************************************/
-package rapture.web
+package rapture.http
 
 import scala.xml._
 import scala.collection.mutable.ListBuffer
