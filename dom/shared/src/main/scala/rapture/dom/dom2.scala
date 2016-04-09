63524422f25932c9d72ffc26ea233b9af9e29829
Jon Pretty <jon.pretty@propensive.com>
2016-04-09 18:46:11 +0200

    Step 1

--- dom/shared/src/main/scala/rapture/dom/dom2.scala
+++ dom/shared/src/main/scala/rapture/dom/dom2.scala
@@ -2,65 +2,7 @@ package rapture.dom2
 
 import language.implicitConversions
 import language.higherKinds
-
-object Dom2 {
-
-  sealed trait ContentType { type Return[_, _] }
-  case class AttributeContent(key: String, value: String) extends ContentType { type Return[T, C] = Tag.Attributed[T, C] }
-  case class TagContent[T](node: Any) extends ContentType { type Return[T, C] = Tag.Full[T, C] }
-
-  trait IsContent[-T, +Out <: ContentType]
-
-  implicit def use[E, Out <: ContentType, Return, C](e: E)(implicit ev: IsContent[E, Out]): Content[Out, Out, Return, C] = ???
- 
-  implicit def stringIsContent[T]: IsContent[String, TagContent[T]] = ???
-
-  object Content
-  
-  case class Content[+ElemType <: ContentType, +Applicable <: ContentType, -Return, +Children]()
-
-  object Tag {
-    implicit def tagIsContent[T, C]: IsContent[Tag[_, C], TagContent[T]] = ???
-    implicit def tagIsContent2[T, C]: IsContent[List[Tag[_, C]], TagContent[T]] = ???
-  
-    object Empty
-
-    case class Empty[This, Children]() extends Tag[This, Children](Nil, Nil) {
-      def apply[T <: ContentType, Return, C](elems: Content[T, ContentType, Return, C]*): T#Return[_, C] = ???
-    }
-  
-    object Attributed
-  
-    case class Attributed[This, Children](override val attributes: Seq[Attribute]) extends Tag[This, Children](attributes, Nil) {
-      def apply[T <: ContentType, Return, C](elements: Content[T, TagContent[_], Return, C]*): T#Return[_, C] = ???
-    }
-
-    object Full
-
-    case class Full[This, Children](override val attributes: Seq[Attribute], override val children: Seq[Tag[_, _]]) extends Tag[This, Children](attributes, children) {
-  
-    }
-
-  }
-  
-  class Tag[This, Children](val attributes: Seq[Attribute], val children: Seq[Tag[_, _]])
-
-  object Attribute {
-    implicit def attributeContent: IsContent[Attribute, AttributeContent] = ???
-  }
-  
-  class Attribute(val key: String, value: String)
-
-  val att = new Attribute("key", "value")
-
-  object Table extends Tag.Empty[One, Two]()
-  object Tbody extends Tag.Empty[Two, Three]()
-  object Tr extends Tag.Empty[Three, Four]()
-  object Td extends Tag.Empty[Four, Five]()
-  object Th extends Tag.Empty[Four, Five]()
-
-}
-
+import language.existentials
 
 trait One
 trait Two extends One
@@ -68,12 +10,112 @@ trait Three extends Two
 trait Four extends Three
 trait Five extends Four
 
-object DomTest {
-  import Dom2._
+/*object DomTest {
+  import Dom._
 
-  Tbody(att)
-  Tbody(use(List(Tr, Tr, Tr(att))))
-  Tbody(att)(Tr, Td)
+  object Table extends Node.Empty[One, Two]("table")
+  object Tbody extends Node.Empty[Two, Three]("tbody")
+  object Tr extends Node.Empty[Three, Four]("tr")
+  object Td extends Node.Empty[Four, Five]("td")
+  object Th extends Node.Empty[Four, Five]("th")
+
+  def att = Attribute("key", "value")
+  
+  //Table(Tbody(Tr))
+  //Tbody(Tr, Tr, Tr)
+  //Tbody(att)(Tr, Td)
   
 }
 
+
+object Dom {
+
+  implicit def conv[R[_, _], F](tag: F)(implicit apVal: Apply[F, R]): Content[F, R] =
+    new Content[F, R] {
+      def values = List(tag)
+      def ap = apVal
+    }
+  
+  object Apply {
+
+    implicit def attributeApply: Apply[Attribute, Node.Attributed] =
+      new Apply[Attribute, Node.Attributed] {
+        
+	def apply[T, C](name: String, attributes: Seq[Attribute]): Node.Attributed[T, C] =
+	  Node.Attributed[T, C](name, attributes)
+      }
+    
+    implicit def contentApply[T, C]: Apply[Node[T, C], Node.Full] =
+      new Apply[Node[T, C], Node.Full] {
+        
+	def apply[T2, C2](name: String, nodes: Seq[Node[T, C]]): Node.Full[T2, C2] =
+	  Node.Full[T2, C2](name, Nil, nodes)
+      }
+  }
+
+  trait Apply[O, R[_, _]] {
+    def apply[T, C](name: String, xs: Seq[O]): R[T, C]
+  }
+
+  trait Content[O, R[_, _]] {
+    def values: Seq[O]
+    def ap: Apply[O, R]
+  }
+
+  case class Attribute(key: String, value: String)
+
+  trait Node[This, Children]
+
+  object Node {
+    case class Empty[This, Children](name: String) extends Node[This, Children] {
+      def apply[From, R[_, _]](content: Content[From, R]*): R[This, Children] = content.head.ap.apply(name, content.flatMap(_.values))
+    }
+    
+    case class Attributed[This, Children](name: String, attributes: Seq[Attribute]) extends Node[This, Children] {
+      def apply(content: (Content[Node[This, Children], R] forSome { type R[_, _] })*): Full[This, Children] = Full(name, attributes, content)
+    }
+
+    case class Full[This, Children](name: String, attributes: Seq[Attribute], content: Seq[Any]) extends Node[This, Children]
+  }
+}
+
+*/
+object Dom2 {
+
+  implicit def conv(a: Attribute): Content[Attribute, Node.Attributed] = Content(List(a))
+  implicit def conv2(n: Node.General): Content[Node.General, Node.Full] = Content(List(n))
+ 
+  case class Content[From, To](values: List[From])
+
+  case class Attribute(key: String, value: String)
+
+  object Node {
+    
+    sealed trait General
+
+    case class Empty(name: String) extends General {
+      def apply[From, To](content: Content[From, To]*): To = ???
+    }
+    
+    case class Attributed(name: String, attributes: Seq[Attribute]) extends General {
+      def apply(content: Content[General, Full]*): Full = Full(name, attributes, content.to[List])
+    }
+
+    case class Full(name: String, attributes: Seq[Attribute], content: Seq[Any]) extends General
+  }
+
+}
+
+object Dom2Test {
+  import Dom2._
+
+  val Table: Node.Empty = Node.Empty("table")
+  val Tbody: Node.Empty = Node.Empty("tbody")
+  val Tr: Node.Empty = Node.Empty("tr")
+  val Td: Node.Empty = Node.Empty("td")
+
+  val att = Attribute("key", "value")
+
+  val tab1 = Table(Tbody(Tr))
+  val tab2 = Table(att)(Tbody(att)(Tr, Tr(att), Tr(Td)))
+}
