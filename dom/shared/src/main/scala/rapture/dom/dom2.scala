c216f74bed31b7b5faafe56abe2639dce4db3fa5
Jon Pretty <jon.pretty@propensive.com>
2016-04-11 18:18:01 +0200

    Supporting attributes too

--- dom/shared/src/main/scala/rapture/dom/dom2.scala
+++ dom/shared/src/main/scala/rapture/dom/dom2.scala
@@ -14,78 +14,86 @@ trait Five
 
 object Dom2 {
 
-  trait Content[That, This, Child, Return] {
+  trait Content[That, ThatAtts, This, Child, Atts, Return] {
     type Position
     
     def value: Position
 
-    def returnValue(empty: Node.Empty[That, This], elements: Seq[Position]): Return
+    def returnValue(empty: Node.Empty[That, ThatAtts, This], elements: Seq[Position]): Return
   }
 
-  case class AttributeContent[That, This, Child](
-    attribute: Attribute
-  ) extends Content[That, This, Child, Node.Attributed[That, This]] {
+  case class AttributeContent[That, ThatAtts, This, Child, Atts](
+    attribute: Attribute[ThatAtts]
+  ) extends Content[That, ThatAtts, This, Child, Atts, Node.Attributed[That, ThatAtts, This]] {
    
-    type Position = Attribute
+    type Position = Attribute[ThatAtts]
     
-    def value: Attribute = attribute
+    def value: Attribute[ThatAtts] = attribute
     
     def returnValue(
-      empty: Node.Empty[That, This],
-      attributes: Seq[Attribute]
-    ): Node.Attributed[That, This] =
+      empty: Node.Empty[That, ThatAtts, This],
+      attributes: Seq[Attribute[ThatAtts]]
+    ): Node.Attributed[That, ThatAtts, This] =
       Node.Attributed(empty.name, attributes)
   }
 
-  case class DomContent[That, This, Child](
-    nodes: Seq[Node.DomNode[This, Child]]
-  ) extends Content[That, This, Child, Node.Full[That, This]] {
+  case class DomContent[That, ThatAtts, This, Child, Atts](
+    nodes: Seq[Node.DomNode[This, Atts, Child]]
+  ) extends Content[That, ThatAtts, This, Child, Atts, Node.Full[That, ThatAtts, This]] {
     
-    type Position = Seq[Node.DomNode[This, Child]]
+    type Position = Seq[Node.DomNode[This, Atts, Child]]
 
-    def value: Seq[Node.DomNode[This, Child]] = nodes
+    def value: Seq[Node.DomNode[This, Atts, Child]] = nodes
 
     def returnValue(
-      empty: Node.Empty[That, This],
-      nodes: Seq[Seq[Node.DomNode[This, Child]]]
-    ): Node.Full[That, This] =
+      empty: Node.Empty[That, ThatAtts, This],
+      nodes: Seq[Seq[Node.DomNode[This, Atts, Child]]]
+    ): Node.Full[That, ThatAtts, This] =
       Node.Full(empty.name, Nil, nodes.flatten)
   }
   
-  implicit def embedNodes[From, That, This, Child](from: From)(implicit embeddable: Embeddable[From, This, Child]): DomContent[That, This, Child] =
+  implicit def embedNodes[From, That, ThatAtts, This, Atts, Child](from: From)(implicit embeddable: Embeddable[From, This, Atts, Child]): DomContent[That, ThatAtts, This, Child, Atts] =
     DomContent(embeddable.embed(from))
     
-  implicit def embedAttributes[That, This, Child](from: Attribute): AttributeContent[That, This, Child] =
+  implicit def embedAttributes[That, ThatAtts, This, Child, Atts](from: Attribute[ThatAtts]): AttributeContent[That, ThatAtts, This, Child, Atts] =
     AttributeContent(from)
     
 
-  object Embeddable {
+  object Embeddable extends Embeddable_1 {
 
-    implicit def domNodeEmbeddable[Type, Child]: Embeddable[Node.DomNode[Type, Child], Type, Child] =
-      new Embeddable[Node.DomNode[Type, Child], Type, Child] { def embed(from: Node.DomNode[Type, Child]) = Seq(from) }
+    implicit def domNodeEmbeddable[Type, Atts, Child]:
+        Embeddable[Node.DomNode[Type, Atts, Child], Type, Atts, Child] =
+      new Embeddable[Node.DomNode[Type, Atts, Child], Type, Atts, Child] {
+        def embed(from: Node.DomNode[Type, Atts, Child]) = Seq(from)
+      }
   
-    implicit def stringEmbeddable[S: StringSerializer] =
-      new Embeddable[S, String, String] {
-        def embed(from: S) = Seq(Node.Text(implicitly[StringSerializer[S]].serialize(from)))
+    implicit def stringEmbeddable[Type, Atts, Child, From: StringSerializer]: Embeddable[From, Type, Atts, Child] =
+      new Embeddable[From, Type, Atts, Child] {
+        def embed(from: From) =
+	  Seq(Node.Text(implicitly[StringSerializer[From]].serialize(from)))
       }
   }
 
-  trait Embeddable[-From, Type, Child] {
-    def embed(from: From): Seq[Node.DomNode[Type, Child]]
+  trait Embeddable_1 {
+    //implicit def 
   }
 
-  case class Attribute(key: String, value: String) {
+  trait Embeddable[-From, Type, Atts, Child] {
+    def embed(from: From): Seq[Node.DomNode[Type, Atts, Child]]
+  }
+
+  case class Attribute[Att](key: String, value: String) {
     override def toString = s"""$key="$value""""
   }
 
   object Node {
     
-    sealed trait DomNode[This, Child] extends Product with Serializable
+    sealed trait DomNode[This, Atts, Child] extends Product with Serializable
 
-    sealed trait Element[This, Child] extends DomNode[This, Child] {
+    sealed trait Element[This, Atts, Child] extends DomNode[This, Atts, Child] {
       def name: String
-      def attributes: Seq[Attribute]
-      def children: Seq[DomNode[Child, _]]
+      def attributes: Seq[Attribute[Atts]]
+      def children: Seq[DomNode[Child, _, _]]
       
       override def toString = {
         val atts = if(attributes.isEmpty) "" else attributes.mkString(" ", " ", "")
@@ -93,34 +101,39 @@ object Dom2 {
       }
     }
 
-    case class Empty[This, Child](name: String) extends Element[This, Child] {
+    case class Empty[This, Atts, Child](name: String) extends Element[This, Atts, Child] {
       
-      def apply[Return](contents: Content[This, Child, _, Return]*): Return = {
+      def apply[Return](contents: Content[This, Atts, Child, _, _, Return]*): Return = {
 	val head = contents.head
 	head.returnValue(this, contents.map(_.value.asInstanceOf[head.Position]))
       }
       
-      def children = Seq[DomNode[Child, _]]()
+      def children = Seq[DomNode[Child, _, _]]()
       def attributes = Seq()
 
       override def toString = s"<$name/>"
     }
     
-    case class Attributed[This, Child](name: String, attributes: Seq[Attribute]) extends Element[This, Child] {
+    case class Attributed[This, Atts, Child](
+      name: String,
+      attributes: Seq[Attribute[Atts]]
+    ) extends Element[This, Atts, Child] {
       
-      def children = Seq[DomNode[Child, _]]()
+      def children = Seq[DomNode[Child, _, _]]()
       
-      def apply[Child2](content: DomContent[This, Child, Child2]*): Full[This, Child] =
+      def apply[Grandchild, ChildAtts](
+        content: DomContent[This, Atts, Child, Grandchild, ChildAtts]*
+      ): Full[This, Atts, Child] =
         Full(name, attributes, content.flatMap(_.nodes))
     }
 
-    case class Full[This, Child](
+    case class Full[This, Atts, Child](
       name: String,
-      attributes: Seq[Attribute],
-      children: Seq[DomNode[Child, _]]
-    ) extends Element[This, Child]
+      attributes: Seq[Attribute[Atts]],
+      children: Seq[DomNode[Child, _, _]]
+    ) extends Element[This, Atts, Child]
 
-    case class Text(content: String) extends DomNode[String, String] {
+    case class Text[Type, Atts, Child](content: String) extends DomNode[Type, Atts, Child] {
       override def toString = content
     }
 
@@ -128,19 +141,21 @@ object Dom2 {
 
 }
 
+trait MyAtt
+
 object Dom2Test {
   import Dom2._
 
-  val Table: Node.Empty[String, Symbol] = Node.Empty("table")
-  val Tbody: Node.Empty[Symbol, Int] = Node.Empty("tbody")
-  val Tr: Node.Empty[Int, Boolean] = Node.Empty("tr")
-  val Td: Node.Empty[Boolean, String] = Node.Empty("td")
+  val Table: Node.Empty[String, MyAtt, Symbol] = Node.Empty("table")
+  val Tbody: Node.Empty[Symbol, MyAtt, Int] = Node.Empty("tbody")
+  val Tr: Node.Empty[Int, MyAtt, Boolean] = Node.Empty("tr")
+  val Td: Node.Empty[Boolean, MyAtt, String] = Node.Empty("td")
 
-  val att = Attribute("key", "value")
+  val att = Attribute[MyAtt]("key", "value")
 
-  val tab1: Node.Full[String, Symbol] = Table(Tbody(Tr(Td, Td(att))))
-  //val tab2 = Table(1, Tbody(att)(Tr, Tr(att), Tr(Td)))
-  //val tab3 = Table(att)(Tbody(att)(Tr, Tr(att), Tr(Td("Hello world"))))
+  val tab1: Node.Full[String, MyAtt, Symbol] = Table(Tbody(Tr(Td, Td(att))))
+  val tab2 = Table(1, Tbody(att)(Tr, Tr(att), Tr(Td)))
+  val tab3 = Table(att)(Tbody(att)(Tr, Tr(att), Tr(Td("Hello world"))))
 
 
 }
