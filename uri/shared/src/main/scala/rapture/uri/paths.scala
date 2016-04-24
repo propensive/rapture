73ecc7c4e5f4285e0a2d7198c9ad0cbf250ca5a2
Jon Pretty <jon.pretty@propensive.com>
2016-04-24 19:17:16 +0200

    Fixed broken paths.scala file

--- uri/shared/src/main/scala/rapture/uri/paths.scala
+++ uri/shared/src/main/scala/rapture/uri/paths.scala
@@ -14,6 +14,8 @@
   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and limitations under the License.
 */
+
+
 package rapture.uri
 
 import language.implicitConversions
